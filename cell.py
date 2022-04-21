a0af2b12a006304423cc8ca770b00099e0947704
haguy77 <idelchikhaguy@gmail.com>
2022-04-21 20:13:06 +0300

    added adjusted size of buttons, Cell.all list, MINES_COUNT formula

--- cell.py
+++ cell.py
@@ -1,10 +1,26 @@
 from tkinter import Button
+import utils
+import random
+import settings
 
 
 class Cell:
-    def __init__(self, is_mine=False):
+    all = []
+
+    def __init__(self, x, y, is_mine=False):
+        """
+        Builder of Cell object
+        :param x: x location on cells' grid
+        :param y: y location on cells' grid
+        :param is_mine: is the cell a mine (Boolean)
+        """
         self.is_mine = is_mine
         self.cell_btn_object = None
+        self.x = x
+        self.y = y
+
+        # Append the object to the Cell.all list
+        Cell.all.append(self)
 
     def create_btn_object(self, location):
         """
@@ -13,7 +29,9 @@ class Cell:
         """
         btn = Button(
             location,
-            text='Text'
+            width=utils.cell_width(),
+            height=utils.cell_height(),
+            text=f"{self.x},{self.y}"
         )
         btn.bind('<Button-1>', self.left_click_actions)  # Left Click
         btn.bind('<Button-3>', self.right_click_actions)  # Right Click
@@ -26,3 +44,14 @@ class Cell:
     def right_click_actions(self, event):
         print(event)
         print("I am right clicked!")
+
+    @staticmethod
+    def randomize_mines():
+        picked_cells = random.sample(
+            Cell.all, settings.MINES_COUNT
+        )
+        for picked_cell in picked_cells:
+            picked_cell.is_mine = True
+
+    def __repr__(self):
+        return f"Cell({self.x}, {self.y})"
--- utils.py
+++ utils.py
@@ -17,3 +17,19 @@ def width_prct(percentage: float) -> float:
     :return: the part of the width according to the percentage
     """
     return (settings.WIDTH / 100) * percentage
+
+
+def cell_height() -> int:
+    """
+    Calculates height of the cell according to settings.HEIGHT
+    :return: height of the cell
+    """
+    return settings.HEIGHT // 180
+
+
+def cell_width() -> int:
+    """
+    Calculates width of the cell according to settings.WIDTH
+    :return: width of the cell
+    """
+    return settings.WIDTH // 90
