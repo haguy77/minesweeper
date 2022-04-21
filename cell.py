6d66a7a3dc47a025a43795c6c550b80ce65d301d
haguy77 <idelchikhaguy@gmail.com>
2022-04-21 21:23:50 +0300

    added functions for returning all neighbor cells and changing text to number of close mines

--- cell.py
+++ cell.py
@@ -31,7 +31,7 @@ class Cell:
             location,
             width=utils.cell_width(),
             height=utils.cell_height(),
-            text=f"{self.x}, {self.y}"
+            # text=f"{self.x}, {self.y}"
         )
         btn.bind('<Button-1>', self.left_click_actions)  # Left Click
         btn.bind('<Button-3>', self.right_click_actions)  # Right Click
@@ -43,8 +43,45 @@ class Cell:
         else:
             self.show_cell()
 
+    def get_cell_by_axis(self, x: int, y: int):
+        """
+        Return a cell object of the value of x, y
+        :param x: x place in grid
+        :param y: y place in grid
+        :return: cell object of the value of x, y
+        """
+        if 0 > x > settings.GRID_SIZE or 0 > y > settings.GRID_SIZE:
+            return None
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    @property
+    def surrounded_cells(self):
+        cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1),
+        ]
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+
+    @property
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter = counter + 1
+
+        return counter
+
     def show_cell(self):
-        pass
+        self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
 
     def show_mine(self):
         # A logic to interrupt the game and display a message that player lost!
