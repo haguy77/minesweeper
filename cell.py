f75b69a6e76018543d9a08e331b8face41cb05ea
haguy77 <idelchikhaguy@gmail.com>
2022-04-23 17:18:09 +0300

    added finished game message box and quit game

--- cell.py
+++ cell.py
@@ -71,6 +71,10 @@ class Cell:
                 for cell_obj in self.surrounded_cells:
                     cell_obj.show_cell()
             self.show_cell()
+            # If Mine count is equal to cells left count, player won
+            if Cell.cell_count == settings.MINES_COUNT:
+                ctypes.windll.user32.MessageBoxW(0, "Congratulations !\nYou won the game !", "Game Over", 0)
+                sys.exit()
 
         # Cancel Left and Right click events if cell is already opened:
         self.cell_btn_object.unbind("<Button-1>")
